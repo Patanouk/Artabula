0fa36d9c508a3fc8f6d1db6240f704403d9c7c47
Jean Lainee <jean.lainee@gmail.com>
2023-07-23 16:27:53 +0800

    Add docker script to start mysql server

---
+++ docker-command.sh
@@ -0,0 +1,12 @@
+docker container rm -f wikimedia-mysql
+
+docker container run \
+      -d \
+      --name wikimedia-mysql \
+      -e MYSQL_USER=jean \
+      -e MYSQL_PASSWORD=jean \
+      -e MYSQL_ALLOW_EMPTY_PASSWORD=yes \
+      -e MYSQL_DATABASE=wikimedia \
+      -p 3306:3306 \
+      -v "${PWD}"/monuments_db.sql:/docker-entrypoint-initdb.d/monuments_db.sql \
+      mysql:8.0.33
